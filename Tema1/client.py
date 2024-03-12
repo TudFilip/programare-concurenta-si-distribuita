fc5b385e51ddbdaf9b09c0a99a99b837cc23b81a
TudFilip <filiptudormihail@yahoo.com>
2024-03-12 13:43:31 +0200

    gata tema 1

---
+++ Tema1/client.py
@@ -0,0 +1,120 @@
+import socket
+import time
+
+HOST = "127.0.0.1"
+PORT_TO_PROTOCOL = {
+    "TCP": 8001,
+    "UDP": 8002,
+}
+STREAM_SERVER = "STREAM_SERVER"
+STOP_AND_WAIT_SERVER = "STOP_AND_WAIT_SERVER"
+
+class TCPClient:
+    def __init__(self, host, port, file_name, size, ack):
+        self.__host = host
+        self.__port = port
+        self.__file_name = file_name
+        self.__size = size
+        self.__sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.__ack = ack
+
+    def start(self):
+        self.__sock.connect((self.__host, self.__port))
+        print(f"TCP Client: Connected to {self.__host}:{self.__port}")
+
+        start_time = time.time()
+
+        bytes_written = 0
+        count = 0
+
+        with open(f"data/client/{self.__file_name}", "rb") as test_file:
+            while True:
+                data = test_file.read(self.__size)
+                if not data:
+                    print("TCP Client: Finish transmitting the file")
+                    self.__sock.close()
+                    print("TCP Client: Closing connection")
+                    break
+
+                self.__sock.send(data)
+
+                if self.__ack == STOP_AND_WAIT_SERVER:
+                    print(f"TCP Client: Waiting for server confirmation on chunk {count}")
+                    while len(self.__sock.recv(1)) < 0:
+                        pass
+                    print(f"TCP Client: Confirmation received")
+
+                count += 1
+                bytes_written += len(data)
+                print(f"TCP Client: Sent {len(data)} bytes")
+
+        stop_time = time.time()
+        print(f"TCP Client: It took {stop_time - start_time} seconds")
+        print(f"TCP Client: Sent {count} chunks")
+        print(f"TCP Client: Sent {bytes_written} bytes")
+
+
+class UDPClient:
+    def __init__(self, host, port, file_name, size, ack):
+        self.__host = host
+        self.__port = port
+        self.__file_name = file_name
+        self.__size = size
+        self.__ack = ack
+        self.__sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.__communication_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.__communication_socket.bind((self.__host, self.__port + 1))
+        self.__communication_socket.settimeout(3)
+
+    def start(self):
+        start_time = time.time()
+
+        bytes_written = 0
+        count = 0
+        with open(f"data/client/{self.__file_name}", "rb") as test_file:
+            while True:
+                data = test_file.read(self.__size)
+
+                if len(data) == 0:
+                    print("UDP Client: Finish transmitting the file")
+                    self.__sock.sendto(data, (self.__host, self.__port))
+                    self.__sock.close()
+                    print("UDP Client: Closing connection")
+                    break
+
+                self.__sock.sendto(data, (self.__host, self.__port))
+
+                if self.__ack == STOP_AND_WAIT_SERVER:
+                    print(f"UDP Client: Waiting for server confirmation on chunk {count}")
+                    # data, _ = self.__communication_socket.recvfrom(1)
+                    while len(self.__communication_socket.recvfrom(1)) < 0:
+                        pass
+                    print(f"UDP Client: Confirmation received")
+
+                count += 1
+                bytes_written += len(data)
+
+                print(f"UDP Client: Sent {len(data)} bytes")
+
+        stop_time = time.time()
+        print(f"UDP Client: It took {stop_time - start_time} seconds")
+        print(f"UDP Client: Sent {count} chunks")
+        print(f"UDP Client: Sent {bytes_written} bytes")
+
+
+def start_client(protocol, file_path, chunk_size, ack):
+    if protocol == "TCP":
+        print("TCP Client Start Running")
+        tcp_client = TCPClient(HOST, PORT_TO_PROTOCOL.get("TCP"), file_path, chunk_size, ack)
+        tcp_client.start()
+    elif protocol == "UDP":
+        print("UDP Client Start Running")
+        udp_client = UDPClient(HOST, PORT_TO_PROTOCOL.get("UDP"), file_path, chunk_size, ack)
+        udp_client.start()
+
+
+# start_client("TCP", "Curs1.pdf", 65000, STREAM_SERVER)
+# start_client("UDP", "Curs1.pdf", 1024, STREAM_SERVER)
+# start_client("UDP", "linuxmint-21.2-cinnamon-64bit.iso", 1024, STREAM_SERVER)
+# start_client("UDP", "Curs1.pdf", 1024, STOP_AND_WAIT_SERVER)
+start_client("UDP", "linuxmint-21.2-cinnamon-64bit.iso", 1024, STOP_AND_WAIT_SERVER)
---
+++ Tema1/server.py
@@ -0,0 +1,195 @@
+import socket
+import threading
+from datetime import datetime
+
+HOST = "127.0.0.1"
+PORT_TO_PROTOCOL = {
+    "TCP": 8001,
+    "UDP": 8002,
+}
+STREAM_SERVER = "STREAM_SERVER"
+STOP_AND_WAIT_SERVER = "STOP_AND_WAIT_SERVER"
+
+class TCPServer:
+    def __init__(self, host, port, size, ack):
+        self.__host = host
+        self.__port = port
+        self.__size = size
+        self.__ack = ack
+        self.__sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.__sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.__sock.bind((self.__host, self.__port))
+
+    def start(self):
+        self.__sock.listen(2)
+
+        print(f"TCP Server: Listening on {self.__host}:{self.__port}")
+
+        while True:
+            conn, addr = self.__sock.accept()
+            conn.settimeout(60)
+            if self.__ack == STREAM_SERVER:
+                threading.Thread(target=self.__start_as_stream, args=(conn, addr,)).start()
+            else:
+                threading.Thread(target=self.__start_as_stop_and_wait, args=(conn, addr,)).start()
+
+    def __start_as_stream(self, conn, addr):
+        client_ip = addr[0]
+        print(f"TCP Server: Client {client_ip} connected")
+
+        count = 0
+        bytes_read = 0
+
+        now = datetime.now()
+        current_timestamp = now.strftime("%d-%m-%Y_%H-%M")
+        with open(f"data/server/TCP_STREAM_{current_timestamp}.pdf", "wb") as out:
+            try:
+                while True:
+                    data = conn.recv(self.__size)
+
+                    if not data:
+                        print("TCP Server: Done reading data from client")
+                        conn.close()
+                        break
+
+                    count = count + 1
+                    bytes_read = bytes_read + len(data)
+                    out.write(data)
+            except:
+                print("TCP Server: Error during receiving data")
+        print("TCP Server: Received {} chunks".format(count))
+        print("TCP Server: Received {} bytes".format(bytes_read))
+
+    def __start_as_stop_and_wait(self, conn, addr):
+        client_ip = addr[0]
+        print(f"TCP Server: Client {client_ip} connected")
+
+        count = 0
+        bytes_read = 0
+        _data = None
+
+        now = datetime.now()
+        current_timestamp = now.strftime("%d-%m-%Y_%H-%M")
+        with open(f"data/server/TCP_STOP_AND_WAIT_{current_timestamp}.pdf", "wb") as out:
+            try:
+                while True:
+                    data = conn.recv(self.__size)
+
+                    if not data:
+                        print("TCP Server: Done reading data from client")
+                        conn.close()
+                        break
+
+                    out.write(data)
+
+                    bytes_read += len(data)
+                    count += 1
+
+                    print("TCP Server: Sending ack bytes")
+                    ack_bytes = bytearray()
+                    ack_bytes.append(1)
+                    conn.send(ack_bytes)
+
+                    print(f"TCP Server: Send client a confirmation message for chunk {count}")
+            except:
+                print("TCP Server: Error during receiving data")
+        print("TCP Server: Received {} chunks".format(count))
+        print("TCP Server: Received {} bytes".format(bytes_read))
+
+
+class UDPServer:
+    def __init__(self, host, port, size, ack):
+        self.__host = host
+        self.__port = port
+        self.__size = size
+        self.__ack = ack
+        self.__sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.__sock.bind((self.__host, self.__port))
+
+    def start_as_stop_and_wait(self):
+        count = 0
+        bytes_read = 0
+
+        now = datetime.now()
+        current_timestamp = now.strftime("%d-%m-%Y_%H-%M")
+        with open(f"data/server/UDP_STOP_AND_WAIT_{current_timestamp}.iso", "wb") as out:
+            try:
+                _data = None
+                while True:
+                    data, addr = self.__sock.recvfrom(self.__size)
+
+                    if not data:
+                        print("UDP Server: Done reading data from client")
+                        break
+
+                    out.write(data)
+
+                    bytes_read += len(data)
+                    if data != _data:
+                        print(f"UDP Server: Received {len(data)} bytes")
+                        _data = data
+                        count += 1
+
+                    print("UDP Server: Sending ack bytes")
+
+                    ip, port = addr
+                    ack_bytes = bytearray()
+                    ack_bytes.append(1)
+                    self.__sock.sendto(ack_bytes, (ip, self.__port + 1))
+
+                    print(f"UDP Server: Send client a confirmation message for chunk {count}")
+            except:
+                print("UDP Server: Error during receiving data")
+                print(f"UDP Server: Received {count} messages")
+
+        print(f"UDP Server: Received {count} chunks")
+        print(f"UDP Server: Received {bytes_read} bytes")
+
+    def start_as_stream(self):
+        count = 0
+        bytes_read = 0
+
+        now = datetime.now()
+        current_timestamp = now.strftime("%d-%m-%Y_%H-%M")
+        with open(f"data/server/UDP_STREAM_{current_timestamp}.iso", "wb") as out:
+            try:
+                while True:
+                    data, addr = self.__sock.recvfrom(self.__size)
+
+                    if not data:
+                        print("UDP Server: Done reading data from client")
+                        break
+
+                    bytes_read += len(data)
+                    count += 1
+                    print(f"UDP Server: Received {len(data)} bytes")
+
+                    out.write(data)
+            except:
+                print("UDP Server: Error during receiving data")
+                print(f"UDP Server: Received {count} messages")
+
+        print(f"UDP Server: Received {count} chunks")
+        print(f"UDP Server: Received {bytes_read} bytes")
+
+
+def start_server(protocol, chunk_size, ack):
+    if protocol == "TCP":
+        print("TCP Server Start Running")
+        tcp_server = TCPServer(HOST, PORT_TO_PROTOCOL.get("TCP"), chunk_size, ack)
+        tcp_server.start()
+    elif protocol == "UDP":
+        print("UDP Server Start Running")
+        udp_server = UDPServer(HOST, PORT_TO_PROTOCOL.get("UDP"), chunk_size, ack)
+        if ack == STREAM_SERVER:
+            udp_server.start_as_stream()
+        elif ack == STOP_AND_WAIT_SERVER:
+            udp_server.start_as_stop_and_wait()
+    else:
+        print("Wrong protocol")
+
+
+# start_server("TCP", 65000, STREAM_SERVER)
+# start_server("TCP", 65000, STOP_AND_WAIT_SERVER)
+# start_server("UDP", 65000, STREAM_SERVER)
+start_server("UDP", 65000, STOP_AND_WAIT_SERVER)
